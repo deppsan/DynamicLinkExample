50c0cf85f53c70d8cc6f96843e7ef67082521f4e
deppsan <ricardo.achg@gmail.com>
2023-02-23 16:21:32 -0600

    [MOD]
    Adjust to functions

--- app/src/main/java/com/example/shareanddynamiclinksexample/MainActivity.kt
+++ app/src/main/java/com/example/shareanddynamiclinksexample/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.shareanddynamiclinksexample
 
+import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.applyCanvas
+import androidx.core.view.ViewCompat
 import com.google.firebase.FirebaseApp
-import com.google.firebase.dynamiclinks.ktx.*
+import com.google.firebase.dynamiclinks.ktx.androidParameters
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.dynamiclinks.ktx.shortLinkAsync
+import com.google.firebase.dynamiclinks.ktx.socialMetaTagParameters
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.ktx.initialize
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,26 +26,35 @@ class MainActivity : AppCompatActivity() {
 
         btnButton.setOnClickListener {
 
-            FirebaseApp.initializeApp(applicationContext)
-            Firebase.dynamicLinks.shortLinkAsync {
-                val baseUrl = "https://www.mioxxo.com"
-                val PATH_SEGMENT = "/cupones_segment"
-                val LINKS_URL_PREFIX = "https://shareanddynamiclinksexample.page.link"
+            val baseUrl = "https://www.mioxxo.com"
+            val PATH_SEGMENT = "/cupones_segment"
+            val LINKS_URL_PREFIX = "https://shareanddynamiclinksexample.page.link"
 
-                link = Uri.parse("${baseUrl + PATH_SEGMENT}")
-                domainUriPrefix = LINKS_URL_PREFIX
-                androidParameters { }
-                socialMetaTagParameters {
-                    title = ""
-                    description = ""
-                    imageUrl = Uri.parse("") //Imagen metadata al compartir
-                }
-            }.addOnSuccessListener {
-                tvDynamicLink.text = it?.shortLink.toString()
+            generateShortDynamicLink(baseUrl, PATH_SEGMENT, LINKS_URL_PREFIX, tvDynamicLink)
+        }
+    }
+
+    private fun generateShortDynamicLink(
+        baseUrl: String,
+        PATH_SEGMENT: String,
+        LINKS_URL_PREFIX: String,
+        tvDynamicLink: TextView
+    ) {
+        FirebaseApp.initializeApp(applicationContext)
+        Firebase.dynamicLinks.shortLinkAsync {
+
+            link = Uri.parse("${baseUrl + PATH_SEGMENT}")
+            domainUriPrefix = LINKS_URL_PREFIX
+            androidParameters { }
+            socialMetaTagParameters {
+                title = ""
+                description = ""
+                imageUrl = Uri.parse("") //Imagen metadata al compartir
             }
-            .addOnFailureListener {
-                tvDynamicLink.text = it?.message
-            }
+        }.addOnSuccessListener {
+            tvDynamicLink.text = it.shortLink.toString()
+        }.addOnFailureListener {
+            tvDynamicLink.text = it.message
         }
     }
 }
